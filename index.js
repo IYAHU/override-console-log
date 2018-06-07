864be9373c441393e8ab4a0bee1f72e1e738451d
Shammi Hans <shammihans1@gmail.com>
2018-06-07 14:23:38 +0530

    removing require keyword
    removing require keyword,
    placeholders to argumentArray,
    git url updated

--- index.js
+++ index.js
@@ -9,7 +9,56 @@ console.logI('info format');         // [2018-4-5 16:43:23] INFO info format
 console.logD('debug fromat');        // [2018-4-5 16:43:23] DEBUG debug fromat
 console.logE('Error format');        // [2018-4-5 16:43:23] ERROR Error format
 */
-const console = require('logger');
+
+var origlog = console.log;
+
+console.log = function (obj, ...argumentArray) {
+    var dateString = new Date().toLocaleString();
+    if (typeof obj === 'string')
+        argumentArray.unshift(`[${dateString}] ${obj}`);
+    else {
+        // This handles console.log( object )
+        argumentArray.unshift(obj);
+        argumentArray.unshift(`[${dateString}]`);
+    }
+    origlog.apply(this, argumentArray);
+};
+
+console.logD = function (obj, ...argumentArray) {
+    var dateString = new Date().toLocaleString();
+    if (typeof obj === 'string')
+        argumentArray.unshift(`[${dateString}] DEBUG ${obj}`);
+    else {
+        // This handles console.log( object )
+        argumentArray.unshift(obj);
+        argumentArray.unshift(`[${dateString}] DEBUG`);
+    }
+    origlog.apply(this, argumentArray);
+};
+
+console.logI = function (obj, ...argumentArray) {
+    var dateString = new Date().toLocaleString();
+    if (typeof obj === 'string')
+        argumentArray.unshift(`[${dateString}] INFO ${obj}`);
+    else {
+        // This handles console.log( object )
+        argumentArray.unshift(obj);
+        argumentArray.unshift(`[${dateString}] INFO`);
+    }
+    origlog.apply(this, argumentArray);
+};
+
+console.logE = function (obj, ...argumentArray) {
+    var dateString = new Date().toLocaleString();
+    if (typeof obj === 'string')
+        argumentArray.unshift(`[${dateString}] ERROR ${obj}`);
+    else {
+        // This handles console.log( object )
+        argumentArray.unshift(obj);
+        argumentArray.unshift(`[${dateString}] ERROR`);
+    }
+    origlog.apply(this, argumentArray);
+};
 
 console.log('Overriding console.log is enabled');
 
